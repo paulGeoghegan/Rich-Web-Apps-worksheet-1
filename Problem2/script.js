eee9a4119ec83e4a6c352fd4428ac3ced8734a41
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2022-10-03 11:57:41 +0100

    Update script.js
    Almost finished part 4

--- Problem2/script.js
+++ Problem2/script.js
@@ -2,10 +2,19 @@ let index = 0;
 
 document.getElementById("addNote").addEventListener("click", function(){
 	let newNote = document.getElementById("note").value
-	document.getElementById("notes").innerHTML+=`<div id=`+index+`> `+newNote+` <input type="button" value="delete" onclick(removeNote(`+index+`))/> </div>`
+	document.getElementById("notes").innerHTML+=`<div id=`+index+`> <p id="text`+index+`"> `+newNote+` <input type="button" value="delete" onclick="removeNote(`+index+`)"/> <input type="button" value="edit" onclick="editNote(`+index+`)"/> </div>`
 	index++
 });
 
 function removeNote(elementToRemove) {
 	document.getElementById(elementToRemove).remove();
+}
+
+function editNote(elementToEdit) {
+	document.getElementById(elementToEdit).innerHTML+=`<div id="editingPopup`+elementToEdit+`"> <input id="popup`+elementToEdit+`" type="text" value="`+document.getElementById(elementToEdit).innerText+`"/><input type="button" value="save" onclick="saveEdit(`+elementToEdit+`)" /> </div>`
+}
+
+function saveEdit(newElement) {
+	document.getElementById("text"+newElement).innerText=document.getElementById("popup"+newElement).value
+	document.getElementById("editingPopup"+newElement).remove
 }
